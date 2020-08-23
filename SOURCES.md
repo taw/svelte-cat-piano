f297c7663dbf90f26aa27bd25613a88e21f34c14
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2020-08-23 15:54:42 +0100

    Added second sound

--- SOURCES.md
+++ SOURCES.md
@@ -1,1 +1,2 @@
 * `public/sounds/Kitten_Meow-SoundBible.com-1295572573.mp3` - http://soundbible.com/1286-Kitten-Meow.html
+* `110011__tuberatanka__cat-meow.mp3` - https://freesound.org/people/tuberatanka/sounds/110011/
--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,18 @@
 <script>
-let audio = new Audio("sounds/Kitten_Meow-SoundBible.com-1295572573.mp3");
+  let audio = [
+    new Audio("sounds/Kitten_Meow-SoundBible.com-1295572573.mp3"),
+    new Audio("sounds/110011__tuberatanka__cat-meow.mp3"),
+  ];
 
-function clicked() {
-  audio.currentTime = 0;
-  audio.play();
-}
+  function clicked(index) {
+    audio[index].currentTime = 0;
+    audio[index].play();
+  }
 </script>
 
-<button on:click={clicked}>
-  Meow Please
-</button>
-
-
 <style>
+
 </style>
+
+<button on:click={() => clicked(0)}>Meow Please</button>
+<button on:click={() => clicked(1)}>C</button>
