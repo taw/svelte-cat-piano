ec82ab9b78ac9eab57fa3228f3e32de0d3307871
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2020-08-23 16:01:48 +0100

    Two more sounds

--- SOURCES.md
+++ SOURCES.md
@@ -1,2 +1,4 @@
 * `public/sounds/Kitten_Meow-SoundBible.com-1295572573.mp3` - http://soundbible.com/1286-Kitten-Meow.html
 * `110011__tuberatanka__cat-meow.mp3` - https://freesound.org/people/tuberatanka/sounds/110011/
+* `362652__trngle__cat-meow.mp3` - https://freesound.org/people/TRNGLE/sounds/362652/
+* `446025__sophiemezam__meow.mp3` - https://freesound.org/people/SophieMezaM/sounds/446025/
--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,8 @@
   let audio = [
     new Audio("sounds/Kitten_Meow-SoundBible.com-1295572573.mp3"),
     new Audio("sounds/110011__tuberatanka__cat-meow.mp3"),
+    new Audio("sounds/362652__trngle__cat-meow.mp3"),
+    new Audio("sounds/446025__sophiemezam__meow.mp3")
   ];
 
   function clicked(index) {
@@ -15,4 +17,6 @@
 </style>
 
 <button on:click={() => clicked(0)}>Meow Please</button>
-<button on:click={() => clicked(1)}>C</button>
+<button on:click={() => clicked(1)}>Meow 2</button>
+<button on:click={() => clicked(2)}>Meow 3</button>
+<button on:click={() => clicked(3)}>Meow 4</button>
